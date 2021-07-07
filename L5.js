2aed4df14a7a877a958212cae1b0f8a604812469
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-07-08 01:30:13 +0600

    Adding color

---
+++ L5.js
@@ -0,0 +1,10 @@
+const dataSet = [1,2,3,4,5,6,7];
+
+d3.select('body')
+.selectAll('h2')
+.data(dataSet)
+.enter()
+.append('h2')
+.text((d) => d + 'USD')
+.style('font-family','verdana')
+.style('color','red')
